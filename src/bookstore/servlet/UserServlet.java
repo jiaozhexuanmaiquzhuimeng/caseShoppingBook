7d0b4d92b7b31d286b842eb1d6a46ffdfbf59831
jiaozhexuanmaiquzhuimeng <31786737+jiaozhexuanmaiquzhuimeng@users.noreply.github.com>
2018-03-17 14:33:52 +0800

    登陆跳转
    登陆跳转

---
+++ src/bookstore/servlet/UserServlet.java
@@ -0,0 +1,51 @@
+package bookstore.servlet;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * @author Guozhen_Zhao
+ * 创建时间：2018年3月17日  下午2:22:45
+ * 备注：
+ */
+@WebServlet("/userServlet")
+public class UserServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String methodName = request.getParameter("method");
+
+		try {
+			Method method = getClass().getDeclaredMethod(methodName, HttpServletRequest.class,
+					HttpServletResponse.class);
+			//获取私有成员变量
+			method.setAccessible(true);
+			method.invoke(this, request, response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException(e);
+			
+		}
+	}
+	
+	public void loginPage(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException{
+		request.getRequestDispatcher("/WEB-INF/jsp/denglu.jsp").forward(request, response);
+	}
+
+}
